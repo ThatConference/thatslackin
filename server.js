949d08f1ef0d9a4144d05892472d70ada884cbee
Brett Slaski <brettski@yahoo.com>
2017-01-29 17:53:24 -0600

    clean up

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // That conference slackin
 var port = process.env.PORT || 8080;
 var slacktoken = process.env.SLACK_TOKEN || "-1"
-console.log('slacktoken' + slacktoken);
 
 require('slackin').default({
   token: slacktoken,
